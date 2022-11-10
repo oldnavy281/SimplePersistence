4c81bbc8bd2a52a790cb695b2922e85b7961e8a3
oldnavy281 <oldnavy281@gmail.com>
2022-11-10 12:55:32 -0700

    index

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 var fs = require('fs');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+PrintPeopleDetails = (path) => {
+    const input = prompt("Please input the file path you wish to use: ");
+    path = input;
+}
