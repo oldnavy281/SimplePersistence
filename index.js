6514792875d282af4a6be57f81ed8e7c2f871a5f
oldnavy281 <oldnavy281@gmail.com>
2022-11-10 13:00:05 -0700

    i dont need a name fuck you

--- index.js
+++ index.js
@@ -22,3 +22,8 @@ PrintPeopleDetails = (path) => {
     const input = prompt("Please input the file path you wish to use: ");
     path = input;
 }
+
+PrintEmployee = (path) => {
+    const input = prompt("Please input the file path you wish to use: ");
+    path = input;
+}
