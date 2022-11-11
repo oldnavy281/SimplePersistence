60b5b63ca43f6e888c136f8f21521e1f80d37296
oldnavy281 <oldnavy281@gmail.com>
2022-11-11 15:06:18 -0700

    pain 2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var fs = require('fs');
+var prompt = require('prompt');
 
 
 
@@ -19,20 +20,44 @@ class Employee {
 
 
 PrintPeopleDetails = (path) => {
-    const idInput = prompt("Please input the ID you are looking for: ");
-    path = "C:\Users\tjoyner\Database2\people\simple";
+    const idInput = prompt.start("Please input the ID you are looking for (1-10): ");
+    this.path = path;
     file = path + ``
 
     for (i = 0; i < path.length; i++) {
-        
         fs.readFile(path, function(err, data){
             if (err){
                 return console.log(err);
             }
             console.log(data);
         })
+        switch (idInput) {
+            case "1":
+                console.log(fs.readFile(file));
+            case "2":
+                console.log(fs.readFile(file));
+            case "3":
+                console.log(fs.readFile(file));
+            case "4":
+                console.log(fs.readFile(file));
+            case "5":
+                console.log(fs.readFile(file));
+            case "6":
+                console.log(fs.readFile(file));
+            case "7":
+                console.log(fs.readFile(file));
+            case "8":
+                console.log(fs.readFile(file));
+            case "9":
+                console.log(fs.readFile(file));
+            case "10":
+                console.log(fs.readFile(file));
+            default:
+                return prompt.start("Please reenter a valid ID number (1-10)");
+        }
     }
-    fs.readFile = path + `${idInput}.txt`;
+    var finalFile = fs.readFile(path + `${idInput}.txt`);
+    console.log(finalFile);
 }
 
 PrintEmployee = (path) => {
@@ -42,4 +67,4 @@ PrintEmployee = (path) => {
     fs.readFile = path + `${idInput}.txt`;
 }
 
-console.log(PrintPeopleDetails);
+PrintPeopleDetails('C:\Users\cstgeorges\Desktop\Databases II\people\simple');
