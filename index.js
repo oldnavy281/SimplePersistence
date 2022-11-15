4c8d9eeb43f033b884ec3d9c4a7273992f4d1c3e
oldnavy281 <oldnavy281@gmail.com>
2022-11-15 16:41:57 -0700

    THANK FUCK

--- index.js
+++ index.js
@@ -20,29 +20,37 @@ class Employee {
 
 
 PrintPeopleDetails = (path) => {
-    const idInput = prompt.start("Please input the ID you are looking for (1-10): ");
+    // const idInput = prompt.start("Please input the ID you are looking for (1-10): ");
+    const idInput = "2";
     this.path = path;
-    file = path + "" + `${idInput}.txt`
+    file = path + "\\" + `${idInput}.txt`;
+    console.log("This is the thing please work");
+    console.log(file);
 
     for (i = 0; i < path.length; i++) {
-        fs.readFile(path, function(err, data){
+        fs.readFile(file, "utf-8", function(err, data){
             if (err){
+                console.log("Got an error")
                 return console.log(err);
             } else if (idInput > 0 && idInput < 11) {
-                console.log(fs.readFile(file));
+                console.log("DATA:", data)
             }
             console.log(data);
         })
     }
-    var finalFile = fs.readFile(file);
-    console.log(finalFile);
+
+    // var finalFile = fs.readFile(file, function () {
+    //     console.log("We got a file!")
+    //     console.log(finalFile);
+    // });
+    
 }
 
 PrintEmployee = (path) => {
     const idInput = prompt("Please input the ID you are looking for: ");
-    path = "C:\Users\cstgeorges\Documents\GitHub\SimplePersistence\people\simple";
+    path = "C:\\Users\\cstgeorges\\Documents\\GitHub\\SimplePersistence\\people\\simple";
 
-    fs.readFile = path + `${idInput}.txt`;
+    fs.readFile = file;
 }
 
-PrintPeopleDetails("C:\Users\cstgeorges\Documents\GitHub\SimplePersistence\people\simple");
+PrintPeopleDetails("C:\\Users\\cstgeorges\\Documents\\GitHub\\SimplePersistence\\people\\simple");
